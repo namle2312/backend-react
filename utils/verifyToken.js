cdcef1af41e0e35916edc16e369e575cacc124e8
Le Ngoc Nam <yo231269@gmail.com>
2023-08-16 16:10:28 +0700

    cm3

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -34,7 +34,7 @@ export const verifyUser = (req, res, next) => {
 
 export const verifyAdmin = (req, res, next) => {
     verifyToken(req, res, next, () => {
-        if (req.user.role === 'admin') {
+        if (req.user.role === 'user') {
             next();
         } else {
             return res
